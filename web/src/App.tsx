7b7be457d09b4435b7c83a316a68c776e13c03a1
rafacoelho4 <rafaelmalto@hotmail.com>
2020-10-22 21:06:13 -0300

    Frontend finalizado

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import Home from './pages/Home';
+
+import Routes from './routes';
+
+const App = () => {
+  
+  return (
+    <div>
+      <Routes />
+    </div>
+  );
+}
+
+export default App;
